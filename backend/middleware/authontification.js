51e68169e53048b45112f5c33590c0ea9819340d
Sardor977 <sarerkinov@gmail.com>
2025-03-10 14:02:37 +0500

    ...

--- backend/middleware/authontification.js
+++ backend/middleware/authontification.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const authenticate = (req, res, next) => {
+exports.authenticate = (req, res, next) => {
     const token = req.headers['authorization'];
     if (!token) return res.status(401).json({ message: 'No token' });
     jwt.verify(token, 'secret_key', (err, decoded) => {
@@ -9,6 +9,5 @@ const authenticate = (req, res, next) => {
         next();
     });
 };
-module.exports = authenticate;
 
 
