ff4b5a80153f157b6694c355c371b63ba8c86be2
Ibrohimov_Zafar <ibragimovzafar001@gmail.com>
2025-03-10 13:46:28 +0500

    kursQoshish api va barcha kurslar  api tayyor

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -2,20 +2,34 @@ const pool = require("../config/db")
 
 exports.addCourse = async (req, res) => {
     try {
-        const { name, category, image_url, price } = req.body;
-        const trainer_id = req.user.id;
-        const result = await pool.query('INSERT INTO Course (TrainerID, CourseName, Category, ImageURL, Price) VALUES ($1, $2, $3, $4, $5)')
-        [TrainerID,CourseName,Category,ImageURL,Price]
-
+        const { CourseName, Category, ImageURL, Price } = req.body;
+        const TrainerID = req.user.id;
+        const result = await pool.query('INSERT INTO Course (TrainerID, CourseName, Category, ImageURL, Price) VALUES ($1, $2, $3, $4, $5) RETURNING *'
+        [TrainerID, CourseName, Category, ImageURL, Price]);
+        res.status(201).json(result.rows[0]);
     } catch (error) {
+        res.status(400).json('Kurs qo`shishda muammo bo`lib qoldi')
 
     }
 }
 
 exports.seeTeacherCourse = async (req, res) => {
     try {
+        const TrainerID = req.user.id;
+        const result = await pool.query(`
+        SELECT 
+            c.id AS CourseID,
+            c.CourseName,
+            c.Category,
+            c.ImageURL,
+            c.Price
+        FROM Course c
+        WHERE c.TrainerID = ${TrainerID};
+`)
 
+res.status(200).json(result.rows)
     } catch (error) {
+        res.status(404).json('Serverda muammo')
 
     }
 }
