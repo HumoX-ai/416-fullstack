95e03811378bb13585f8805f13326323508ae038
Sardor977 <sarerkinov@gmail.com>
2025-03-10 13:33:03 +0500

    signup blan loginup qoshdm

---
+++ backend/controllers/studentController.js
@@ -0,0 +1,30 @@
+const pool = require('../config/db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.signupStudent = async (req, res) => {
+    const { FirstName, LastName, Username, Password, Age } = req.body;
+    const hashedPassword = await bcrypt.hash(Password, 10);
+    try {
+        const result = await pool.query(
+            'INSERT INTO Student (FirstName, LastName, Username, Password, Age) VALUES ($1, $2, $3, $4, $5) returning id, username',
+            [FirstName, LastName, Username, hashedPassword, Age]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        res.status(400).json({ message: 'Username already exists' });
+    }
+};
+
+exports.loginStudent = async (req, res) => {
+    const { Username, Password } = req.body;
+    const result = await pool.query('SELECT * FROM Student WHERE Username = $1', [Username]);
+    const student = result.rows[0];
+    if (!student || !(await bcrypt.compare(Password, student.Password))) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: student.id, role: 'student' }, 'secret_key', { expiresIn: '1h' });
+    res.json({ token });
+};
+
+module.exports = { signupStudent, loginStudent };
--- backend/middleware/authontification.js
+++ backend/middleware/authontification.js
@@ -1,12 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const authenticate = (req, res, next) => {
-  const token = req.headers['authorization'];
-  if (!token) return res.status(401).json({ message: 'No token' });
-  jwt.verify(token, 'secret_key', (err, decoded) => {
-    if (err) return res.status(403).json({ message: 'Invalid token' });
-    req.user = decoded;
-    next();
-  });
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ message: 'No token' });
+    jwt.verify(token, 'secret_key', (err, decoded) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+        req.user = decoded;
+        next();
+    });
 };
 module.exports = authenticate;
+
+
